ca086d40050c165b07b70c86dbe8aa473c21b596
josetrespalaciosb <josetrespalaciosbedoya@gmail.com>
2025-02-21 12:10:28 -0500

    Style basics

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<h1>
 <?php
     echo "hola mundo";
 ?>
+</h1>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
